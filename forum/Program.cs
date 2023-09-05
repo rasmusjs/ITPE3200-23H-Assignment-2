eae3a8eee49963ae8ca7ab591a4cf72ed2d25541
rasmusjs <rasmus.skramstad@gmail.com>
2023-09-05 14:12:31 +0200

    New base based on "HomeView, Navigation and Routing" from week 2

--- forum/Program.cs
+++ forum/Program.cs
@@ -1,35 +1,9 @@
-using Microsoft.EntityFrameworkCore;
-using forum.Models;
-
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<TopicDbContext>(options =>
-{
-    options.UseSqlite(builder.Configuration["ConnectionStrings:TopicDbContextConnections"]);
-});
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
+app.MapDefaultControllerRoute();
 
 app.Run();
