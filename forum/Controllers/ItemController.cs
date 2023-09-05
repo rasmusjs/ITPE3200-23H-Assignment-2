0cabd2e6c7f9f397d660871491c82bfc56a4fb56
rasmusjs <rasmus.skramstad@gmail.com>
2023-09-05 14:17:39 +0200

    Added Item.cs

---
+++ forum/Controllers/ItemController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using forum.Models;
+
+namespace forum.Controllers;
+
+public class ItemController : Controller
+{
+    public IActionResult Table()
+    {
+        var items = new List<Item>();
+        var item1 = new Item();
+        item1.ItemId = 1;
+        item1.Name = "Pizza";
+        item1.Price = 60;
+
+        var item2 = new Item
+        {
+            ItemId = 2,
+            Name = "Fried Chicken Leg",
+            Price = 15
+        };
+
+        items.Add(item1);
+        items.Add(item2);
+
+        ViewBag.CurrentViewName = "List of Shop Items";
+        return View(items);
+    }
+}
---
+++ forum/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace forum.Models
+{
+	public class Item
+	{
+		public int ItemId { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public decimal Price { get; set; }
+		public string? Description { get; set; }
+		public string? ImageUrl { get; set; }
+	}
+}
+
