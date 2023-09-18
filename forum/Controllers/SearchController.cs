f11e46a1c3ddd086e194a00f5a7344a957f81caa
jrgn9 <jorsko2@gmail.com>
2023-09-18 12:06:26 +0200

    Commented all the SearchController code

--- forum/Controllers/SearchController.cs
+++ forum/Controllers/SearchController.cs
@@ -53,18 +53,22 @@ public class SearchController : Controller
         if (string.IsNullOrWhiteSpace(term)) return Index(); // TODO: Redirect to error page
         if (term.Length < 2) return Index(); // TODO: Redirect to error page
 
-        //
+        // Fetch all posts based on the search term
         var posts = await _postRepository.GetAllPostsByTerm(term);
+        
+        // Error handling if the term does not provide any posts
         if (posts == null)
         {
             _logger.LogError("[ItemController] Item list not found while executing _itemRepository.GetAll()");
             return NotFound("Item list not found");
         }
 
+        // Return view with all the posts matching the search term
         var postsListViewModel = new PostsListViewModel(posts, "Search");
         return View(postsListViewModel);
     }
 
+    // Sends user to index
     public IActionResult Index()
     {
         return View();
