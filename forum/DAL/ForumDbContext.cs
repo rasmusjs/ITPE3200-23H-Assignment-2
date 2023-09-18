f8506494a9a429eae684165a3020a88b71f930e0
jrgn9 <jorsko2@gmail.com>
2023-09-18 12:24:10 +0200

    Commented and cleaned the code in ForumDbContext

--- forum/DAL/ForumDbContext.cs
+++ forum/DAL/ForumDbContext.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace forum.DAL;
 
+// The ORM (Object-Relational Mapper) for the application
+// This is the communication between the application and the database, defining data structures and relationships.
 public class ForumDbContext : IdentityDbContext
 {
     public ForumDbContext(DbContextOptions<ForumDbContext> options) : base(options)
@@ -11,6 +13,7 @@ public class ForumDbContext : IdentityDbContext
         //Database.EnsureCreated();
     }
 
+    // Getter and setters for the models
     //public DbSet<User> Users { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Tag> Tags { get; set; }
@@ -19,43 +22,30 @@ public class ForumDbContext : IdentityDbContext
     //public DbSet<PostTag> PostTags { get; set; }
     public DbSet<Comment> Comments { get; set; }
 
+    // Configuring the relationships and schemas for the entities in the database
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
-        //modelBuilder.Entity<PostTag>().HasKey(pt => new { pt.PostId, pt.TagId });
-
-
-        modelBuilder.Entity<Post>()
-            .HasMany(p => p.Tags)
-            .WithMany(t => t.Posts)
-            .UsingEntity(j => j.ToTable("PostTag"));
-
-
-        //https://learn.microsoft.com/en-us/ef/core/modeling/relationships/many-to-many
+        // Configuring the many to many relationship between tags and posts
+        // Source: https://learn.microsoft.com/en-us/ef/core/modeling/relationships/many-to-many
         // Link Posts and Tags using the help table PostTag
-
         modelBuilder.Entity<Post>()
             .HasMany(p => p.Tags)
             .WithMany(t => t.Posts)
             .UsingEntity(j => j.ToTable("PostTag"));
 
-
-        // Link Posts with Categories
+        // Configuring the one-to-many relationship between Posts and Categories
         modelBuilder.Entity<Post>().HasOne(p => p.Category).WithMany().HasForeignKey(p => p.CategoryId);
 
-
-        // Link Comments with Posts
-        //modelBuilder.Entity<Comment>().HasOne(c => c.Post).WithMany(p => p.Comments).HasForeignKey(c => c.PostId);
-
-
+        // Configuring the self-referencing relationship for Comments (For replies to comments)
         modelBuilder.Entity<Comment>()
             .HasMany(c => c.CommentReplies)
             .WithOne(c => c.ParentComment)
             .HasForeignKey(c => c.ParentCommentId);
-        //modelBuilder.Entity<Post>().HasOne(p => p.User).WithMany(u => u.Posts).HasForeignKey(p => p.UserId);
     }
 
+    // Enable Lazy Loading for loading data when it is needed
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseLazyLoadingProxies();
