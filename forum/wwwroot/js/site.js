a810fdaa5079cf9775ba1466564e104113f5b73a
Ole-Kristian-Haaseth-Rudjord <s354564@oslomet.no>
2023-09-19 10:23:37 +0200

    Added onclick eventlistener for button on navbar (currently not working)

--- forum/wwwroot/js/site.js
+++ forum/wwwroot/js/site.js
@@ -1,1 +1,9 @@
-﻿
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const burgerBtn = document.getElementById("burger-btn");
+    const ul = document.getElementById("nav-link-list");
+
+    burgerBtn.addEventListener("click", function () {
+        ul.classList.toggle("open");
+        burgerBtn.classList.toggle("active");
+    });
+});
