6f7f4c25dc5838b3cf96251b837f716fbe739e96
rasmusjs <rasmus.skramstad@gmail.com>
2023-09-26 14:27:05 +0200

    Fixed typo in password length

--- forum/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ forum/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -96,7 +96,7 @@ namespace forum.Areas.Identity.Pages.Account
             /// </summary>
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
-                MinimumLength = 6)]
+                MinimumLength = 8)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
