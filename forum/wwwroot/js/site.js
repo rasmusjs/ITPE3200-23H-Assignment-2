6f7cc15384b32eb145f9795365c4fbcf45e54778
ole-kristian-rudjord <s354564@oslomet.no>
2023-10-11 11:27:51 +0200

    Added styling and functionality to comments

--- forum/wwwroot/js/site.js
+++ forum/wwwroot/js/site.js
@@ -8,4 +8,32 @@
         burgerBtn.classList.toggle("active");
         navLinkList.classList.toggle("active");
     })
+
+    document.querySelectorAll(".reply-btn").forEach(function(button) {
+        button.addEventListener("click", function() {
+            // Toggle the "active" class on the related div
+            this.nextElementSibling.classList.toggle("active");
+
+            // Toggle button text between original and "Close"
+            if (this.textContent === "Reply") {
+                this.textContent = "Close";
+            } else {
+                this.textContent = "Reply";
+            }
+        });
+    });
+
+    document.querySelectorAll(".edit-btn").forEach(function(button) {
+        button.addEventListener("click", function() {
+            // Toggle the "active" class on the related div
+            this.nextElementSibling.classList.toggle("active");
+
+            // Toggle button text between original and "Close"
+            if (this.textContent === "Edit") {
+                this.textContent = "Close";
+            } else {
+                this.textContent = "Edit";
+            }
+        });
+    });
 })
