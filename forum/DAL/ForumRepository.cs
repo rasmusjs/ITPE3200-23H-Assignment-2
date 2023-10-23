c269359bc524964a8563b68382b24db23025bbee
jrgn9 <jorsko2@gmail.com>
2023-10-23 12:29:00 +0200

    Added more logging and error handling

--- forum/DAL/ForumRepository.cs
+++ forum/DAL/ForumRepository.cs
@@ -10,18 +10,18 @@ namespace forum.DAL;
 // A generic repository class for the forum
 public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity : class
 {
-    // DB session and logger
+    // Initialize DB session and logger
     private readonly ForumDbContext _db;
     private readonly ILogger<ForumRepository<TEntity>> _logger;
 
-    // Constructor for initializing the logger
+    // Constructor for initializing the logger and DB
     public ForumRepository(ForumDbContext db, ILogger<ForumRepository<TEntity>> logger)
     {
         _db = db;
         _logger = logger;
     }
 
-    // Fetches all entities from the database
+    // Function for fetching all entities from the database
     public async Task<IEnumerable<TEntity>?> GetAll()
     {
         // Tries to retrieve all records from the database as a list
@@ -32,11 +32,13 @@ public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity 
         // Exception error handling if it can't fetch entities from the database
         catch (Exception e)
         {
+            // Sends function name and error to the LogError function
             LogError("GetAll", e);
             return null;
         }
     }
 
+    // Function to fetch user activity from the database
     public async Task<ApplicationUser?> GetUserActivity(string userId)
     {
         // Checks if the user id is null or empty
@@ -52,11 +54,11 @@ public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity 
         // Exception error handling if it can't fetch entities from the database
         catch (Exception e)
         {
+            // Sends function name and error to the LogError function
             LogError("GetUserActivity", e);
             return null;
         }
     }
-
 
     // Get all posts from database, based on a search term 
     public async Task<IEnumerable<Post>?> GetAllPostsByTerm(string term, string userId = "")
@@ -89,6 +91,7 @@ public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity 
         // Error handling if it can't fetch posts from the search term
         catch (Exception e)
         {
+            // Sends function name and error to the LogError function
             LogError("GetAllPostsByTerm", e);
             return null;
         }
@@ -104,11 +107,13 @@ public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity 
                 .Include(post => post.Comments)
                 .Where(post => post.PostId == id).FirstAsync();
 
+            // If there is a userId, get user data
             if (userId != "")
             {
                 // Fetches the user activity
                 var user = await GetUserActivity(userId);
 
+                // If there is a user
                 if (user != null)
                 {
                     if (user.LikedPosts != null)
@@ -118,6 +123,7 @@ public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity 
                             post.IsLiked = true;
                     }
 
+                    // If the user have liked comments add the likes to them
                     if (user.LikedComments != null && post.Comments != null)
                     {
                         post.Comments = await AddLikeToComments(post.Comments, userId);
@@ -127,9 +133,10 @@ public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity 
 
             return post;
         }
-        // Error handling if it can't fetch the post
+        // Error handling if it can't fetch the post from db
         catch (Exception e)
         {
+            // Sends function name and error to the LogError function
             LogError("GetPostById", e);
             return null;
         }
@@ -148,15 +155,16 @@ public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity 
 
             return posts;
         }
-        // Error handling if it can't fetch posts
+        // Error handling if it can't fetch posts from db
         catch (Exception e)
         {
+            // Sends function name and error to the LogError function
             LogError("GetAllPosts", e);
             return null;
         }
     }
 
-// Fetches comments from the database, based on the comment id
+    // Fetches comments from the database, based on the comment id
     public async Task<IEnumerable<Comment>?> GetCommentsByPostId(int postId, string userId = "")
     {
         try
@@ -170,15 +178,16 @@ public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity 
 
             return comments;
         }
-        // Error handling if it can't fetch the comment
+        // Error handling if it can't fetch the comment from db
         catch (Exception e)
         {
+            // Sends function name and error to the LogError function
             LogError("GetCommentsByPostId", e);
             return null;
         }
     }
 
-// Generic method to fetch any entity based on id
+    // Generic method to fetch any entity based on id
     public async Task<TEntity?> GetTById(int id)
     {
         try
@@ -189,13 +198,14 @@ public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity 
         // Error handling if it can't fetch entities
         catch (Exception e)
         {
+            // Sends function name and error to the LogError function
             LogError("GetTById", e);
             return null;
         }
     }
 
 
-// Generic method to create and save an entity
+    // Generic method to create and save an entity
     public async Task<TEntity?> Create(TEntity entity)
     {
         try
@@ -203,17 +213,19 @@ public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity 
             // Tries to add an entity, save the changes and return the entity
             _db.Set<TEntity>().Add(entity);
             await _db.SaveChangesAsync();
+            _logger.LogInformation("[Forum Repository] Entity added to the database: {entity}", entity);
             return entity;
         }
-        // Error handling if it can't create a new entity
+        // Error handling if it can't create a new entity in the db
         catch (Exception e)
         {
+            // Sends function name and error to the LogError function
             LogError("Create", e);
             return null;
         }
     }
 
-// Generic method to update an entity
+    // Generic method to update an entity
     public async Task<bool> Update(TEntity entity)
     {
         try
@@ -221,17 +233,19 @@ public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity 
             // Tries to update an entity in the database and save the changes
             _db.Set<TEntity>().Update(entity);
             await _db.SaveChangesAsync();
+            _logger.LogInformation("[Forum Repository] Entity updated in the database: {entity}", entity);
             return true;
         }
-        // Error handling if it can't update an entity
+        // Error handling if it can't update an entity in the db
         catch (Exception e)
         {
+            // Sends function name and error to the LogError function
             LogError("Update", e);
             return false;
         }
     }
 
-// Generic method to delete an entity based on id
+    // Generic method to delete an entity based on id
     public async Task<bool> Delete(int id)
     {
         if (id < 0) // id is not valid (negative)
@@ -244,16 +258,18 @@ public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity 
         {
             // Finds the entity in the database
             var entity = await _db.Set<TEntity>().FindAsync(id);
-            // Error handling if there are no entity with the provided id
+            // Error handling if there are no entity with the given id 
             if (entity == null)
             {
-                LogError($"Delete, entity not found for id {id}", new Exception("Entity not found"));
+                // Sends function name and error to the LogError function
+                LogError("Delete", new Exception("Entity not found"));
                 return false;
             }
 
             // Removes the entity from the database and save the changes
             _db.Set<TEntity>().Remove(entity);
             await _db.SaveChangesAsync();
+            _logger.LogInformation("[Forum Repository] Entity removed from the database: {entity}", entity);
             return true;
         }
         // Error handling if it is not able to delete the entity
@@ -264,12 +280,13 @@ public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity 
         }
     }
 
-// Method for deleting all the tags from a post based on entity id
+    // Method for deleting all the tags from a post based on entity id
     public async Task<bool> RemoveAllPostTags(int id)
     {
         if (id < 0) // id is not valid (negative)
         {
-            LogError($"RemoveAllPostTags, entity not found for id {id}", new Exception("Entity not found"));
+            // Sends function name and error to the LogError function
+            LogError("RemoveAllPostTags", new Exception($"Entity not found for id {id}"));
             return false;
         }
 
@@ -285,7 +302,8 @@ public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity 
             // Error handling if it could not find the entity 
             if (executeSqlAsync == 0)
             {
-                LogError($"RemoveAllPostTags, entity not found for id {id}", new Exception("Entity not found"));
+                // Sends function name and error to the LogError function
+                LogError("RemoveAllPostTags", new Exception($"Entity not found for id {id}"));
                 return false;
             }
 
@@ -294,38 +312,81 @@ public class ForumRepository<TEntity> : IForumRepository<TEntity> where TEntity 
         // Error handling if it failed to delete the tags from the entity
         catch (Exception e)
         {
+            // Sends function name and error to the LogError function
             LogError("RemoveAllPostTags", e);
             return false;
         }
     }
 
 
-    private async Task<List<Post>> AddLikeToPosts(List<Post> posts, string userId)
+    private async Task<List<Post>?> AddLikeToPosts(List<Post>? posts, string userId)
     {
-        // Fetches the user activity
-        var user = await GetUserActivity(userId);
+        // Check if posts are null or empty
+        if (posts == null || !posts.Any())
+        {
+            LogError("AddLikeToPosts", new InvalidOperationException("Posts are null or empty"));
+            throw new InvalidOperationException("Posts list cannot be empty");
+        }
 
-        if (user != null && user.LikedPosts != null)
-            // Loops through all posts
-            foreach (var post in posts)
-                // Checks if the user has liked the post
-                if (user.LikedPosts.Any(t => t.PostId == post.PostId))
-                    post.IsLiked = true;
+        try
+        {
+            // Fetches the user activity
+            var user = await GetUserActivity(userId);
+
+            if (user != null && user.LikedPosts != null)
+            {
+                // Loops through all posts
+                foreach (var post in posts)
+                    // Checks if the user has liked the post
+                    if (user.LikedPosts.Any(t => t.PostId == post.PostId))
+                    {
+                        post.IsLiked = true;
+                    }
+            }
+            else
+            {
+                LogError("AddLikeToPosts", new Exception("GetUserActivity() returned null"));
+            }
+        }
+        catch (Exception e)
+        {
+            LogError("AddLikeToPosts", e);
+        }
 
         return posts;
     }
 
-    private async Task<List<Comment>> AddLikeToComments(List<Comment> comments, string userId)
+    private async Task<List<Comment>?> AddLikeToComments(List<Comment>? comments, string userId)
     {
-        // Fetches the user activity
-        var user = await GetUserActivity(userId);
+        // Check if comments are null or empty
+        if (comments == null || !comments.Any())
+        {
+            LogError("AddLikeToComments", new InvalidOperationException("Comments are null or empty"));
+            throw new InvalidOperationException("Comments list cannot be empty");
+        }
 
-        if (user != null && user.LikedComments != null)
-            // Loops through all posts
-            foreach (var comment in comments)
-                // Checks if the user has liked the comment
-                if (user.LikedComments.Any(t => t.CommentId == comment.CommentId))
-                    comment.IsLiked = true;
+        try
+        {
+            // Fetches the user activity
+            var user = await GetUserActivity(userId);
+
+            if (user != null && user.LikedComments != null)
+            {
+                // Loops through all posts
+                foreach (var comment in comments)
+                    // Checks if the user has liked the comment
+                    if (user.LikedComments.Any(t => t.CommentId == comment.CommentId))
+                        comment.IsLiked = true;  
+            }
+            else
+            {
+                LogError("AddLikeToComments", new Exception("GetUserActivity() returned null")); 
+            }
+        }
+        catch (Exception e)
+        {
+            LogError("AddLikeToComments", e); 
+        }
 
         return comments;
     }
