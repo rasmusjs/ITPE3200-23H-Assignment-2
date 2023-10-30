acfc94c27243ef369854483e8f03143dd0b1762e
rasmusjs <rasmus.skramstad@gmail.com>
2023-10-30 14:04:46 +0100

    Fixed warning "Initializing property by default value is redundant".

--- forum/ViewModels/PostViewModel.cs
+++ forum/ViewModels/PostViewModel.cs
@@ -10,7 +10,7 @@ namespace forum.ViewModels;
 public class PostViewModel
 {
     // Property which contains a Post object (forum post)
-    public Post? Post { get; set; } = default!;
+    public Post? Post { get; set; }
 
     // List of SelectListItem objects - The different categories that a forum post can belong to
     public List<SelectListItem> CategorySelectList { get; set; } = default!;
