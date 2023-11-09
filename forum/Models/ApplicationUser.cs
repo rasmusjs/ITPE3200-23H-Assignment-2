bee1c21d6d762a3dbde86c2a9af8ebb8338c8a34
Ole-Kristian-Haaseth-Rudjord <s354564@oslomet.no>
2023-11-09 11:33:38 +0100

    Fixed JsonProperty names

--- forum/Models/ApplicationUser.cs
+++ forum/Models/ApplicationUser.cs
@@ -10,7 +10,7 @@ namespace forum.Models;
 public class ApplicationUser : IdentityUser
 {
     // Get the value from the base class
-    [JsonProperty("userName")] [NotMapped] public override string UserName => base.UserName;
+    [JsonProperty("username")] [NotMapped] public override string UserName => base.UserName;
 
     public DateTime CreationDate { get; set; } = DateTime.Now;
     [JsonProperty("profilePicture")] public byte[]? ProfilePicture { get; set; }
