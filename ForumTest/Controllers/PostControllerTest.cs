69ac7de2a85583e3d532ca9c151dbb269a21ab06
jrgn9 <jorsko2@gmail.com>
2023-11-24 11:25:50 +0100

    Tested GetPost and GetTags, both success and not found

--- ForumTest/Controllers/PostControllerTest.cs
+++ ForumTest/Controllers/PostControllerTest.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using forum.Controllers;
 using forum.DAL;
 using forum.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -24,7 +26,9 @@ public class PostControllerTest
     private readonly Mock<UserManager<ApplicationUser>> _mockUserManager;
     private readonly Mock<IForumRepository<Category>> _mockCategoryRepository;
     private readonly Mock<IForumRepository<Comment>> _mockCommentRepository;
+
     private readonly Mock<ForumDbContext> _mockForumDbContext;
+
     //private readonly Mock<IMemoryCache> _mockMemoryCache;
     private readonly Mock<ILogger<PostController>> _mockLogger;
 
@@ -39,7 +43,7 @@ public class PostControllerTest
         _mockTags = new Mock<IForumRepository<Tag>>();
         _mockCategoryRepository = new Mock<IForumRepository<Category>>();
         _mockCommentRepository = new Mock<IForumRepository<Comment>>();
-        _memoryCache = new MemoryCache(new MemoryCacheOptions());   // New memory cache to avoid having to mocking it 
+        _memoryCache = new MemoryCache(new MemoryCacheOptions()); // New memory cache to avoid having to mocking it 
         _mockLogger = new Mock<ILogger<PostController>>();
 
         // https://stackoverflow.com/questions/37630564/how-to-mock-up-dbcontext
@@ -75,7 +79,8 @@ public class PostControllerTest
         {
             new Post
             {
-                PostId = 1, Title = "Simple ToDo App", Content = "Hello everyone", DateCreated = DateTime.Now.AddDays(-2),
+                PostId = 1, Title = "Simple ToDo App", Content = "Hello everyone",
+                DateCreated = DateTime.Now.AddDays(-2),
                 DateLastEdited = DateTime.Now.AddDays(-2), TotalLikes = 9, UserId = "user1", CategoryId = 1,
                 Tags = new List<Tag> { new() { Name = "Beginner" }, new() { Name = "HTML" } }
             },
@@ -94,42 +99,132 @@ public class PostControllerTest
         };
     }
 
-    // GetAllPosts(string sortby = "")
-   [Fact]
-   public async Task GetAllPosts_ReturnPostsOKTest()
-   {
-       // Arrange
-       var mockPosts = GetMockPosts();
-       _mockPostRepository.Setup(repo => repo.GetAllPosts(It.IsAny<String>())).ReturnsAsync(mockPosts);
-       
-       // Act
-       var result = await _controller.GetAllPosts("likes");
+    // Method for testing GetAllPosts function when it returns OK 
+    [Fact]
+    public async Task GetAllPosts_ReturnPostsOKTest()
+    {
+        // Arrange
+        var mockPosts = GetMockPosts();
+        _mockPostRepository.Setup(repo => repo.GetAllPosts(It.IsAny<String>())).ReturnsAsync(mockPosts);
 
-       // Assert
-       var okResult = Assert.IsType<OkObjectResult>(result);
-       Assert.IsAssignableFrom<IEnumerable<Post>>(okResult.Value);
-   }
-   
-   [Fact]
-   public async Task? GetAllPosts_ReturnPostsNotFoundTest()
-   {
-       // Arrange
-       var emptyPosts = new List<Post>();
-       _mockPostRepository.Setup(repo => repo.GetAllPosts(It.IsAny<String>())).ReturnsAsync(emptyPosts);
-       
-       // Act
-       var result = await _controller.GetAllPosts("likes");
+        // Act
+        var result = await _controller.GetAllPosts("likes");
 
-       // Assert
-       var noPostsFound = Assert.IsType<NotFoundObjectResult>(result);
-       Assert.Equal("No posts found", noPostsFound.Value);
-   }
-   
-   // GetPost(int id)
-   
-   //GetTags()
-   
-   // GetCategories()
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.IsAssignableFrom<IEnumerable<Post>>(okResult.Value);
+    }
+
+    // Method for testing GetAllPosts function when it returns NotFound
+    [Fact]
+    public async Task? GetAllPosts_ReturnPostsNotFoundTest()
+    {
+        // Arrange
+        var emptyPosts = new List<Post>(); // Empty list of posts
+        _mockPostRepository.Setup(repo => repo.GetAllPosts(It.IsAny<String>())).ReturnsAsync(emptyPosts);
+
+        // Act
+        var result = await _controller.GetAllPosts("likes");
+
+        // Assert
+        var noPostsFound = Assert.IsType<NotFoundObjectResult>(result);
+        Assert.Equal("No posts found", noPostsFound.Value);
+    }
+
+    // Method for testing GetPost function when it returns OK
+    [Fact]
+    public async Task GetPost_ReturnPostOkTest()
+    {
+        // Arrange
+        var mockPosts = GetMockPosts();
+        int postId = 1;
+        var userId = "user1";
+        _mockPostRepository.Setup(repo => repo.GetPostById(postId, It.IsAny<string>()))
+            .ReturnsAsync(mockPosts.FirstOrDefault(p => p.PostId == postId));
+
+        // Mock the user id with a claim
+        // Source: https://weblogs.asp.net/ricardoperes/unit-testing-the-httpcontext-in-controllers
+        _controller.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                    { new Claim(ClaimTypes.NameIdentifier, userId) }))
+            }
+        };
+
+        // Act
+        var result = await _controller.GetPost(postId);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnedPost = Assert.IsType<Post>(okResult.Value);
+        Assert.Equal(postId, returnedPost.PostId);
+    }
+
+    // Method for testing GetPost function when it returns NotFound
+    [Fact]
+    public async Task GetPost_ReturnPostNotFoundTest()
+    {
+        // Arrange
+        var emptyPost = new List<Post>(); // Empty post list
+        _mockPostRepository.Setup(repo => repo.GetPostById(It.IsAny<int>(), It.IsAny<string>()))
+            .ReturnsAsync(emptyPost.FirstOrDefault(p => p.PostId == 1));
+
+        // Act
+        var result = await _controller.GetPost(1);
+
+        // Assert
+        var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+        Assert.Equal("Post not found, cannot show post", notFoundResult.Value);
+    }
+    
+    // Method for getting mock tags for testing
+    private List<Tag> GetMockTags()
+    {
+        return new List<Tag>
+        {
+            new Tag {TagId = 1, Name = "Beginner"},
+            new Tag {TagId = 2, Name = "HTML"},
+            new Tag {TagId = 3, Name = "JavaScript"},
+            new Tag {TagId = 4, Name = "Java"}
+        };
+    }
+
+    // Method for testing GetTags function when it returns OK and count amount of tags
+    [Fact]
+    public async Task GetTags_ReturnTagsOkTest()
+    {
+        // Arrange
+        var mockTags = GetMockTags();
+        _mockTags.Setup(repo => repo.GetAll()).ReturnsAsync(mockTags);
+
+        // Act
+        var result = await _controller.GetTags();
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnedTags = Assert.IsType<List<Tag>>(okResult.Value);
+        Assert.Equal(mockTags.Count, returnedTags.Count);
+    }
+    
+    // Method for testing GetTags function when it returns NotFound
+    [Fact]
+    public async Task GetTags_ReturnNotFoundTest()
+    {
+        // Arrange
+        var emptyTags = new List<Tag>(); // Empty tag list
+        _mockTags.Setup(repo => repo.GetAll()).ReturnsAsync(emptyTags);
+
+        // Act
+        var result = await _controller.GetTags();
+
+        // Assert
+        var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+        Assert.Equal("Tags not found", notFoundResult.Value);
+    }
+
+// GetCategories()
    
    // GetComments(int id)
    
