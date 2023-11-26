96a9897c2bd1d33228e96808afc9504dbf1df9cc
rasmusjs <rasmus.skramstad@gmail.com>
2023-11-26 12:48:26 +0100

    Added ' to regex title for posts

--- forum/Models/Post.cs
+++ forum/Models/Post.cs
@@ -14,9 +14,9 @@ public class Post
     [JsonProperty("id")] public int PostId { get; set; }
 
     // Regex for error handling the post title
-    [RegularExpression(@"[0-9a-zA-ZæøåÆØÅ \-/:/?/./!/#]{2,64}",
+    [RegularExpression(@"[0-9a-zA-ZæøåÆØÅ \-/':?.!#]{2,64}",
         ErrorMessage =
-            "The title can only contain numbers, letters or characters -:?.!, and must be between 2 to 64 characters.")]
+            "The title can only contain numbers, letters or characters '-:?.!, and must be between 2 to 64 characters.")]
     [Display(Name = "Title")]
     // Getters and setters for post title
     [JsonProperty("title")]
