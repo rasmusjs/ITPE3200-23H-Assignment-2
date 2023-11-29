ba29cb1f7210bfaa11a99c6eb5376abbcf28a9e5
rasmusjs <rasmus.skramstad@gmail.com>
2023-11-29 10:16:08 +0100

    Remove unused AspNetCore.Authorization. (Replace by another method).

--- forum/Controllers/PostController.cs
+++ forum/Controllers/PostController.cs
@@ -2,7 +2,6 @@
 using forum.DAL;
 using forum.Models;
 using Ganss.Xss;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
